ee22d0cef0d16342623b69ae2a6c4ce15e11e8f4
jehpfvr <cezario.je@gmail.com>
2025-08-13 13:18:52 -0300

    Add SMTP config and email alert sending

--- rotina_diaria.php
+++ rotina_diaria.php
@@ -1,10 +1,12 @@
 <?php
 require_once __DIR__ . '/banco.php';
+require_once __DIR__ . '/email.php';
 
 $mes = (int)date('m');
 $ano = (int)date('Y');
 
 $alerts = check_limits_and_alert($mes, $ano);
 foreach ($alerts as $alert) {
+    enviar_email(SMTP_USER, 'Alerta de limite excedido', $alert);
     echo $alert . PHP_EOL;
 }
